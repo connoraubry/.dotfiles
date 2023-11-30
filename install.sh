2cfe29b93dfde93af6a5be13de8b9b492699b99c
Connor Aubry <connoraubry@Connors-Air.attlocal.net>
2023-11-30 15:20:27 -0800

    adding tmux config

--- install.sh
+++ install.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
 set -euxo pipefail
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+mkdir -p ~/.config/tmux
+cp $SCRIPT_DIR/tmux.conf ~/.config/tmux/tmux.conf
